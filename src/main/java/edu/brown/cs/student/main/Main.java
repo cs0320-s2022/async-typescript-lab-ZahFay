a83f424a4ab5600fa1f10571dfb66091bdc42949
Alzahra <alzahra_fayie@brown.edu>
2022-03-03 17:02:42 -0500

    lmao bye

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -6,14 +6,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import freemarker.template.Configuration;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 
@@ -59,9 +64,11 @@ public final class Main {
 
     // Setup Spark Routes
 
+
     // TODO: create a call to Spark.post to make a POST request to a URL which
     // will handle getting matchmaking results for the input
     // It should only take in the route and a new ResultsHandler
+    Spark.post(); //?
     Spark.options("/*", (request, response) -> {
       String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
       if (accessControlRequestHeaders != null) {
@@ -106,18 +113,26 @@ public final class Main {
    */
   private static class ResultsHandler implements Route {
     @Override
-    public String handle(Request req, Response res) {
+    public String handle(Request req, Response res) throws JSONException {
       // TODO: Get JSONObject from req and use it to get the value of the sun, moon,
       // and rising
       // for generating matches
 
+      JSONObject h = new JSONObject(req.body());
+
+      String sun = h.getString("sun");//?
+      String moon = h.getString("moon"); //?
+      String rising = h.getString("rising"); //??? i know this wrong
+
       // TODO: use the MatchMaker.makeMatches method to get matches
+      List<String> matches = MatchMaker.makeMatches(sun,moon,rising); //store matches
 
       // TODO: create an immutable map using the matches
 
       // TODO: return a json of the suggestions (HINT: use GSON.toJson())
       Gson GSON = new Gson();
-      return null;
+      Map test = ImmutableMap.of("matches",matches);
+      return GSON.toJson(test);
     }
   }
 }
